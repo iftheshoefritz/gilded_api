e9c40141579eda5cab235981d67ce0db646317c8
Fritz Meissner <fritz.meissner@gmail.com>
2018-02-20 23:27:37 +0200

    initial gilded API

---
+++ gilded_api.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+items = [
+  ["Sulfuras, Hand of Ragnaros", 1, 80],
+  ["Backstage passes to a TAFKAL80ETC concert", 11, 1],
+  ["Aged Brie", 11, 1],
+]
+
+get '/items' do
+  items.to_json
+end
+
+get '/items/:id' do |id|
+  items[params['id'].to_i].to_json
+end
